667376c0739ec7ac0f955c67dd796466fdfb4779
Camilo Barreiro <1845@holbertonschool.com>
2020-10-19 17:24:26 +0000

    add other bucle to change number in insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,24 +1,81 @@
 #include "sort.h"
-/*
-*insertion_sort_list- ascending order using the Insertion sort algorithm
-*list - Pointer of kind of struct
-*Return: there are not return
-*/
+/**
+ *  _swap - change number of position
+ * @head: NUmber to indicate to change
+ * @list: doubly linked list
+ * Return - Not return
+ */
+
+void _swap(listint_t **head, listint_t **list)
+{
+	listint_t *tmp_Head, *tmp_Head_next, *tmp_node;
+
+	if (!(*head)->prev)
+		*list = (*head)->next;
+
+	tmp_Head = tmp_node = *head;
+	tmp_Head_next = tmp_Head->next;
+
+	tmp_Head->next = tmp_Head_next->next;
+	tmp_node = tmp_Head->prev;
+	tmp_Head->prev = tmp_Head_next;
+	tmp_Head_next->next = tmp_Head;
+	tmp_Head_next->prev = tmp_node;
+
+	if (tmp_Head_next->prev)
+		tmp_Head_next->prev->next = tmp_Head_next;
+
+
+	if (tmp_Head->next)
+		tmp_Head->next->prev = tmp_Head;
+
+	*head = tmp_Head_next;
+
+}
+
+
+/**
+ * insertion_sort_list - orts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: doubly linked list
+ * Return: Not return
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t  *head, *aux;
+	listint_t  *head, *List_back, *current;
+
+	if (!list || !(*list) || (!((*list)->prev) && !((*list)->next)))
+		return;
 
 	head = *list;
-
 	while (head && head->next)
 	{
 		if (head->n > head->next->n)
 		{
-			tmp = head;
-			printf("%d", head);
-			_swap(&aux, list);
+			current = head;
+
+			_swap(&current, list);
 			print_list(*list);
-			head = tmp
+			head = current;
+			List_back = current;
+
+			while (List_back && List_back->prev)
+			{
+
+				if (List_back->n < List_back->prev->n)
+				{
+					current = List_back->prev;
+
+					_swap(&(current), list);
+
+					print_list(*list);
+					List_back = current->next;
+				}
+
+				List_back = List_back->prev;
+			}
+
 		}
+		head = head->next;
 	}
 }
